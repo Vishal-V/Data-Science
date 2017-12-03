8c03b9d32dcd56c4f130b13cda9f58616f5c3be3
Vishal Vinod <vishal114186@gmail.com>
2017-12-03 23:56:56 +0530

    Created sentimentAnalysis.py

---
+++ Scripts/sentimentAnalysis.py
@@ -0,0 +1,20 @@
+from textblob import TextBlob
+import tweepy
+
+consumer_api = '' #Your consumer api
+consumer_api_secret = '' #Your consumer api secret
+
+access_token = '' #Your access token
+access_token_secret = '' #Your access token secret
+
+auth = tweepy.OAuthHandler(consumer_api, consumer_api_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+
+tweets = api.search('intelligence')
+
+for tweet in tweets:
+	print(tweet)
+	wiki = TextBlob(tweet.text)
+	print(tweet.seniment)
